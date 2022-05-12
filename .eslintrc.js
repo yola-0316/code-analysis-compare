36fc90bc0329af373a34bbcca405c2830dd91a27
Zhi Huang <zhi.huang@compass.com>
2022-05-12 17:36:33 +0800

    chore: adjust eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-    'prettier',
-  ],
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
@@ -17,18 +13,34 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-  ],
+  plugins: ['react', '@typescript-eslint'],
+  settings: {
+    'import/external-module-folders': ['node_modules', 'node_modules/@types'],
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx', '.d.ts'],
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.json', '.ts', '.tsx', '.d.ts'],
+      },
+    },
+  },
   rules: {
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      { js: 'never', jsx: 'never', ts: 'never', tsx: 'never' },
+    ],
+
+    'react/jsx-filename-extension': 'off',
+    'react/react-in-jsx-scope': 'off',
   },
   overrides: [
     {
-      "files": ["*.ts"],
-      "rules": {
+      files: ['*.ts'],
+      rules: {
         'import/no-extraneous-dependencies': 'off',
-      }
-    }
-  ]
+      },
+    },
+  ],
 };
