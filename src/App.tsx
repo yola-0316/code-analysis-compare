eeab3d912ad80eaaefad448e861b2fbd039993d3
Zhi Huang <zhi.huang@compass.com>
2022-05-12 21:26:39 +0800

    feat: add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,22 @@
-import { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0);
+import { Routes, Route, Link } from 'react-router-dom';
 
+import GitHubAction from './pages/github-action';
+
+function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button
-            type="button"
-            onClick={() => setCount((prevCount) => prevCount + 1)}
-          >
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+    <div>
+      <header>
+        <nav>
+          <Link to="/github">GitHub Action</Link>
+        </nav>
       </header>
+      <main>
+        <Routes>
+          <Route path="github" element={<GitHubAction />} />
+        </Routes>
+      </main>
     </div>
   );
 }
---
+++ src/pages/github-action.tsx
@@ -0,0 +1,3 @@
+export default function GitHubAction() {
+  return <div>GitHub Action</div>;
+}
